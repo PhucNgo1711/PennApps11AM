462f17126e119f405e401db1f155bd8341fc7e36
Ac2zoom <ac2zoom@yahoo.com>
2016-01-23 17:38:14 -0500

    Added emergency tracking

--- build/generated/source/r/debug/com/mapbox/mapboxsdk/R.java
+++ build/generated/source/r/debug/com/mapbox/mapboxsdk/R.java
@@ -625,7 +625,7 @@ public final class R {
 		public static final int my_location = 0x7f02005e;
 		public static final int my_location_bearing = 0x7f02005f;
 		public static final int my_location_stale = 0x7f020060;
-		public static final int notification_template_icon_bg = 0x7f020062;
+		public static final int notification_template_icon_bg = 0x7f020063;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f0e008c;
